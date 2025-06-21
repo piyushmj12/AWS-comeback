82637b8a72c6cda8b00dc2059e14328ed10691c4
piyushmj12 <76661538+piyushmj12@users.noreply.github.com>
2025-06-21 17:26:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
 provider "aws" {
-  region = "ap-south-1"
+    region = "us-east-1"  
 }
 
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_instance" "foo" {
+  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  instance_type = "t2.micro"
+  tags = {
+      Name = "TF-Instance"
+  }
 }
